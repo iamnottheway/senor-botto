75e8ba212107fa8c42a815b0cd0cdf7a4cf0e494
Joel Benjamin <joelbenjamin093@gmail.com>
2017-07-23 11:02:42 +0530

    Add files via upload

--- botutils.py
+++ botutils.py
@@ -6,7 +6,7 @@ import requests
 import json
 
 def send_quickreply(token,user_id,text,reply_payload):
-
+    # quick reply for messenger 
     params = {
         "access_token":token,
 
@@ -18,8 +18,14 @@ def send_quickreply(token,user_id,text,reply_payload):
         "quick_replies":reply_payload,
       }
     })
-    requests.post("https://graph.facebook.com/v2.6/me/messages",params=params,
-                data=payload,headers={'Content-type': 'application/json'})
+    requests.post(
+        "https://graph.facebook.com/v2.6/me/messages",
+        params=params,
+        data=payload,
+        headers={
+            'Content-type': 'application/json'
+        }
+    )
 
 def search_gifs(tenor_key,tag_str):
     # looks for tags on tenor
@@ -28,7 +34,6 @@ def search_gifs(tenor_key,tag_str):
         "key":tenor_key
     }
     resp = requests.get("https://api.tenor.com/v1/search",data=data)
-    # gif image : resp.json()['results'][1]['media'][0]['gif']['url']
     gif_list = []
     print(resp.json()['results'][0]['tags'])
     for x in range(0,len(resp.json()['results'])-1):

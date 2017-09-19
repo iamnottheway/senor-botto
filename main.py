9a5ab6b3e4d0a08f3b24c3d2b1929bdf5110e99e
Joel Benjamin <joelbenjamin093@gmail.com>
2017-09-19 10:23:47 +0530

    Removed db,now using globals again
    Using a DB fucks up the bot. Not sure how, will use db in the future IF the bot doesnt fuck!

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ db = client[db_appname]
 # db collection
 users = db.senorbottousers
 
+location = "none"
+
 def get_user_from_db(db_userId,searchKey):
     """ Looks for the specified user in the db and then queries
         the searchKey in it. SearchKey should match any of the doc
@@ -42,31 +44,6 @@ def db_update_and_increment(db_userId,dockey):
     users.update_one({'user_id':db_userId},{'$inc':{'{}'.format(dockey):1}},upsert=True)
 
 #___________________________________________________________________________________________
-
-class Flow(object):
-    """ class that maps functions to key words. Here key words are the payload
-         keys set in the request/payload builder
-    """
-    def __init__( self, user_payload,flow_payload):
-        self.user_payload = user_payload
-        self.flow_payload = flow_payload
-
-    def key_lookup_and_call(self,user_id):
-        """ Looks for the user_payload key in the flow_payload
-              Then checks if the user_payload is equal,  if true calls
-              the function which is mapped to the user_payload
-
-              flow_dict format :
-
-              { 'payload': function_reference,....}
-        """
-        self.user_id = user_id
-        for dict_key in self.flow_payload.keys():
-            if dict_key == self.user_payload:
-                self.flow_payload[dict_key](self.user_id)
-
-
-
 
 
 #____________________________________________________________________________________________
@@ -99,8 +76,9 @@ def webhook():
 # recieve messages and pass it to some function which parses it further
 @app.route('/testbot',methods=['POST'])
 def recieve_incoming_messages():
-    location = "none"
     # just chilling babe!
+    global location
+    location = "none"
     if request.method == "POST":
         output = request.get_json()
         # for normal text messages
@@ -123,7 +101,7 @@ def recieve_incoming_messages():
                             if x['message']['attachments'][0].get('payload'):
                                 if x['message']['attachments'][0]['payload'].get('coordinates'):
                                     location = x['message']['attachments'][0]['payload']['coordinates']
-                                    db_update_document(recipient_id,["location",location])
+                                    # db_update_document(recipient_id,["location",location])
                     if x.get('postback'):  # for postback getstarted button
                         recipient_id = x['sender']['id']
                         if x['postback'].get('payload'):
@@ -135,82 +113,67 @@ def recieve_incoming_messages():
 def respond_back(recipient_id,user_payload,user_message):
     """
     """
-    location = get_user_from_db(recipient_id,"location")
+    global location
+    #location = get_user_from_db(recipient_id,"location")
     if location is not "none":
         # if location has some value set the new payload
         # so that the function is executed
         user_payload = "@ShowTaco"
 
-    flow_dict = {
-                    "@get_started": Show_getStartedBtn,
-                    "@joke":ignore_func,
-                    "@taco":AskUserLocation,
-                    "@ShowTaco":SearchTacoVendor,
-
-
-    }
-
-    if len(user_message) >= 0:
-        # use the flow class
-        flow = Flow(user_payload, flow_dict)
-        # calls the matched function from the dict
-        flow.key_lookup_and_call(recipient_id)
-
-
+    
 def Show_getStartedBtn(user_id):
-    # add the user to the database if the user doesnt exsit
-    cursor_count = users.find({"user_id":user_id}).limit(1).count()
-    # user is added to the DB only ones so if the user is in the db this clause is
-    # not executed.
-    if cursor_count == 0:
-        user_info = {
-                    'user_id':user_id,
-                    'location':'none',
-        }
-        users.insert_one(user_info)
     # set location to None when the bot shows the intro_message
-    db_update_document(recipient_id,["location","none"])
-    intro_message = """ Hola amigo! I'm Senor botto🌮. I can show you some of the best taco restuarants in your city🍽! Or tell you a joke or show something funny.
+    # db_update_document(recipient_id,["location",'none'])
+    global location
+    location = "none"
+    intro_message = """ Hola amigo! I'm Senor botto🌮. I can show you some of the best taco restuarants in the US🍽! Just tap the button below.
                     """
     bot.send_text_message(user_id,intro_message)
     reply_options = [("I want to eat","@taco"),("Make me laugh","@joke")]
     botutils.QuickReply_SendButtons(user_id, "What do you want to do?👇", reply_options)
 
 def AskUserLocation(recipient_id):
+    # add the user to the database if the user doesnt exsit
     botutils.Ask_user_location(recipient_id)
 
 def SearchTacoVendor(recipient_id):
     element_data_list = []
-    location = get_user_from_db(recipient_id,"location") # location is a dict
-    food_data = get_food_data.yelp_search(coords=(location['long'],location['lat'])) # init with key. Done internally
-    packed_results = get_food_data.get_res_info(food_data)
-    if len(packed_results) == 0:
-        bot.send_text_message(recipient_id,"Couldn't find anything in your area🍁.")
-        #update the location to none
-        db_update_document(recipient_id,["location","none"])
-    else:
-        food_data_list = []
-        # building the restaurant data here and packing it into a list
-        for restaurant_num in range(len(packed_results)):
-            # combines cost and ratings in a string. Displayed with address
-            sub_detail_str = "{0}, Ratings:{1}".format(packed_results[restaurant_num][4],
-                                                        packed_results[restaurant_num][1],
-                )
-            # the data is appended to the list
-            food_data_list.append({"data":(packed_results[restaurant_num][0],
-                                           packed_results[restaurant_num][3],
-                                           sub_detail_str,
-                                           "www.google.com"
-                )})
+    global location
+    #location = get_user_from_db(recipient_id,"location") # location is a dict
+    try:
+        food_data = get_food_data.yelp_search(coords=(location['long'],location['lat'])) # init with key. Done internally
+        packed_results = get_food_data.get_res_info(food_data)
+        if len(packed_results) == 0:
+            bot.send_text_message(recipient_id,"Couldn't find anything in your area🍁(works only in the US).")
+            #update the location to none
+            location = "none"
+            #db_update_document(recipient_id,["location","None"])
+        else:
+            food_data_list = []
+            # building the restaurant data here and packing it into a list
+            for restaurant_num in range(len(packed_results)):
+                # combines cost and ratings in a string. Displayed with address
+                sub_detail_str = "{0}, Ratings:{1}".format(packed_results[restaurant_num][4],
+                                                            packed_results[restaurant_num][1],
+                    )
+                # the data is appended to the list
+                food_data_list.append({"data":(packed_results[restaurant_num][0],
+                                               packed_results[restaurant_num][3],
+                                               sub_detail_str,
+                                               "www.google.com"
+                    )})
 
-        bot.send_text_message(recipient_id,"Here's what I've found")
-        ele_payload = ({
-                            "element_data":food_data_list,
-                            "button_data":[{"data":["www.google.com","Learn more🌮"]}]
-                        })
-        botutils.generic_button_send(recipient_id,ele_payload)
-        #update the location to none so that the user doesnt see this again when the start over btn is pressed
-        db_update_document(recipient_id,["location","none"])
+            bot.send_text_message(recipient_id,"Here's what I've found")
+            ele_payload = ({
+                                "element_data":food_data_list,
+                                "button_data":[{"data":["https://www.google.co.in/search?source=hp&q={}".format(packed_results[restaurant_num][0]),"Learn more🌮"]}]
+                            })
+            botutils.generic_button_send(recipient_id,ele_payload)
+            #update the location to none so that the user doesnt see this again when the start over btn is pressed
+            #db_update_document(recipient_id,["location","none"])
+            location = "none"
+    except:
+        print("returning none type")
 
 
 def ignore_func(recipient_id):
